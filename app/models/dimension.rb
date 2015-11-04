9265da532a01589201d12d31e048da9de6a339fb
B Kleinen <kleinen@infrastructure.de>
2015-11-04 21:59:57 +0100

    added a dependent: :destroy for dimension values

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -1,5 +1,5 @@
 class Dimension < ActiveRecord::Base
-  has_many :values
+  has_many :values, dependent: :destroy
   has_many :dimensions_in_polls
   has_many :polls, through: :dimensions_in_polls
 end
