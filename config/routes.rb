7dc7040cae1a546cf66444058935fad9aecba853
B Kleinen <kleinen@infrastructure.de>
2015-11-04 22:42:08 +0100

    changed start page

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
     resources :values , :except => [:index,:show]
   end
   resources :polls
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'polls#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
