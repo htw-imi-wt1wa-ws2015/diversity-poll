101fe08e5db4fcd9d301206ccb4f3d8613ade126
B Kleinen <kleinen@infrastructure.de>
2015-11-04 19:15:16 +0100

    nest values into dimensions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :values
-  resources :dimensions
+
+  resources :dimensions do
+    resources :values
+  end
   resources :polls
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
